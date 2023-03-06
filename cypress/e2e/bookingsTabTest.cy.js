04809c6c8673caed7c6b5c376eb6cd300dba14b2
bgolec <ble@spyro-soft.com>
2023-03-06 11:31:29 +0100

    BookingTabTest updated : Locations and Floorplan select functions added along with Location Map and Bulk Book Desks button assertions

--- cypress/e2e/bookingsTabTest.cy.js
+++ cypress/e2e/bookingsTabTest.cy.js
@@ -3,8 +3,13 @@ import {switchToTab} from "./adminTabTest.cy";
 //Selectors
 let dateInput = '[data-testid=DatePickerWidget-textInput]'
 let datePickerIcon = '[data-testid=DatePickerWidget-calendarButton]'
-let locationsSelect = '.SideBySideGroup---side_by_side > :nth-child(1) > div > div > div > div'
-let workspacesSelect = '.SideBySideGroup---side_by_side > :nth-child(2) > div > div > div > div'
+let locationsSelectLabel = '.SideBySideGroup---side_by_side > :nth-child(1) > div > div > div > div > span'
+let locationSelect = '.SideBySideGroup---side_by_side > :nth-child(1) > div > div > div > div'
+let floorplanSelect = '.SideBySideGroup---side_by_side > :nth-child(2) > div > div > div > div'
+let mapIframe = '.FieldLayout---input_below > .CertifiedSAILExtension---sail_extension > iframe'
+let mapToggleRadioButton = '.RadioSelect---choice_pair > label'
+let dropdownItem = '.TetherComponent---sailcontents > div > div > .MenuWidget---listbox > li > div'
+let bulkBookDesks = '.SideBySideItem---flex_item > .ButtonLayout2---button_layout > .Button---btn_wrapper > button'
 
 describe("Check contents of bookings tab vor variety of users with different privileges", () => {
 
@@ -20,12 +25,32 @@ describe("Check contents of bookings tab vor variety of users with different pri
             .should('be.visible')
         cy.get(Cypress.env("selectors").showObjectsButton)
             .should('be.visible')
-        cy.get(workspacesSelect)
+        cy.get(floorplanSelect)
             .should('be.visible')
-        cy.get(locationsSelect)
-            .should('be.visible')
+        cy.get(locationsSelectLabel)
+            .should('have.text', 'Select a location')
         cy.get(datePickerIcon)
             .should('be.visible')
+        cy.get(mapToggleRadioButton).eq(0)
+            .should('have.text', "Show Map")
+        cy.get(mapToggleRadioButton).eq(1)
+            .should('have.text', "Show Grid")
+        cy.get(bulkBookDesks).children()
+            .should('have.text', "Please select a floorplan first")
+        cy.get(bulkBookDesks)
+            .should('be.disabled')
+
+        //and when
+        chooseLocation("Chicago")
+        chooseFloorplan("One")
+
+        //then
+        cy.get(mapIframe)
+            .should('be.visible')
+        cy.get(bulkBookDesks)
+            .should('have.text', "Bulk Book Desks")
+        cy.get(bulkBookDesks)
+            .should('be.enabled')
     })
 
     it("Check presence of elements for manager user", () => {
@@ -38,14 +63,34 @@ describe("Check contents of bookings tab vor variety of users with different pri
         //then
         cy.get(dateInput)
             .should('be.visible')
-        cy.get(workspacesSelect)
+        cy.get(floorplanSelect)
             .should('be.visible')
-        cy.get(locationsSelect)
+        cy.get(locationsSelectLabel)
             .should('be.visible')
         cy.get(datePickerIcon)
             .should('be.visible')
         cy.get(Cypress.env("selectors").showObjectsButton)
             .should('not.exist')
+        cy.get(mapToggleRadioButton).eq(0)
+            .should('have.text', "Show Map")
+        cy.get(mapToggleRadioButton).eq(1)
+            .should('have.text', "Show Grid")
+        cy.get(bulkBookDesks).children()
+            .should('have.text', "Please select a floorplan first")
+        cy.get(bulkBookDesks)
+            .should('be.disabled')
+
+        //and when
+        chooseLocation("Chicago")
+        chooseFloorplan("One")
+
+        //then
+        cy.get(mapIframe)
+            .should('be.visible')
+        cy.get(bulkBookDesks)
+            .should('have.text', "Bulk Book Desks")
+        cy.get(bulkBookDesks)
+            .should('be.enabled')
     })
 
     it("Check presence of elements for regular user", () => {
@@ -58,13 +103,45 @@ describe("Check contents of bookings tab vor variety of users with different pri
         //then
         cy.get(dateInput)
             .should('be.visible')
-        cy.get(workspacesSelect)
+        cy.get(floorplanSelect)
             .should('be.visible')
-        cy.get(locationsSelect)
+        cy.get(locationsSelectLabel)
             .should('be.visible')
         cy.get(datePickerIcon)
             .should('be.visible')
         cy.get(Cypress.env("selectors").showObjectsButton)
             .should('not.exist')
+        cy.get(mapToggleRadioButton).eq(0)
+            .should('have.text', "Show Map")
+        cy.get(mapToggleRadioButton).eq(1)
+            .should('have.text', "Show Grid")
+        cy.get(bulkBookDesks).children()
+            .should('have.text', "Please select a floorplan first")
+        cy.get(bulkBookDesks)
+            .should('be.disabled')
+
+        //and when
+        chooseLocation("Chicago")
+        chooseFloorplan("One")
+
+        //then
+        cy.get(mapIframe)
+            .should('be.visible')
+        cy.get(bulkBookDesks)
+            .should('have.text', "Bulk Book Desks")
+        cy.get(bulkBookDesks)
+            .should('be.enabled')
     })
+
+    function chooseLocation(location) {
+        cy.get(locationSelect).eq(1).click()
+        cy.get(dropdownItem).contains(location).click()
+        cy.wait(1000)
+    }
+
+    function chooseFloorplan(location) {
+        cy.get(floorplanSelect).eq(2).click()
+        cy.get(dropdownItem).contains(location).click()
+        cy.wait(1000)
+    }
 })
